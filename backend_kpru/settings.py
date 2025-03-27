637acbb6c19d29b18a67480fec896fcadf77e1d9
Sim <sim@thepercentage.asia>
2025-03-27 13:14:34 +0700

    [UPDATED] Cors config

--- backend_kpru/settings.py
+++ backend_kpru/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     "myapp"
 ]
 
@@ -47,8 +48,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'backend_kpru.urls'
 
 TEMPLATES = [
