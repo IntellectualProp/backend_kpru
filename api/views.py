c375fed32aa30cd6606785fbc0c1e48430aeb7a1
Sim <sim@thepercentage.asia>
2025-04-15 10:32:49 +0700

    Update URL pattern to route to getSliderImage view

--- api/views.py
+++ api/views.py
@@ -1,11 +1,14 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from myapp.models import WebsiteSetting
+from rest_framework.response import Response
+from .models import WebsiteSetting
 from .serializers import WebsiteSettingSerializer
 
-
 @api_view(["GET"])
-def getData(request):
-    website_setting = WebsiteSetting.objects.all()
+def getSliderImage(request):
+    # Filter WebsiteSetting objects where tag equals 'slider_homepage'
+    website_setting = WebsiteSetting.objects.filter(tag='slider_homepage')
+    
+    # Serialize the filtered queryset
     serializer = WebsiteSettingSerializer(website_setting, many=True)
+    
     return Response(serializer.data)
