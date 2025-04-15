df0baaf288ed9d5116699c28daad3cfad6536fc4
Sim <sim@thepercentage.asia>
2025-04-15 10:21:34 +0700

    Refactor URL patterns for improved organization and clarity

--- backend_kpru/urls.py
+++ backend_kpru/urls.py
@@ -4,10 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('django/admin/', admin.site.urls),  # Admin panel under /django/admin/
-    path('django/api', include("api.urls")),  # Your app under /django/
-    path('django/', include("myapp.urls")),  # Your app under /django/
+    path('django/', include([
+        path('admin/', admin.site.urls),
+        path('api/', include('api.urls')),
+        path('', include('myapp.urls')),
+    ])),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
